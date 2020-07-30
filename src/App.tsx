4821e465978a5963ab265566f6aaeb5b85540580
ヘイスアヤン <park.ahyung52@gmail.com>
2020-07-31 01:53:24 +0900

    deleted unecessary code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import React from 'react';
-// import logo from './logo.svg';
-// import { Button } from '@material-ui/core';
-// import MyButton from './components/Button';
 import CustomPaginationActionsTable from './components/Table';
 import ValidationTextFields from './components/SearchForm';
 import ButtonAppBar from './components/AppBar';
@@ -15,20 +12,6 @@ function App() {
         <h1>
           申し込み一覧
         </h1>
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-
-        {/* <Button variant="outlined" color="secondary">Hello World</Button> */}
       </header>
       <body>
         <ValidationTextFields />
